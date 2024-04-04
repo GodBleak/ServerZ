bed28586c32ef9a67e45a34eba5baf828f8fd2ff
GodBleak <hello@godbleak.dev>
2024-04-04 15:47:35 -0600

    fix inconsistent  spacing

--- src/generateConfig.ts
+++ src/generateConfig.ts
@@ -1,8 +1,7 @@
 import { config } from "./config.js"
 
 export function generateConfig() {
-    return `
-hostname = "${config.server.serverName}";
+    return `hostname = "${config.server.serverName}";
 password = "${config.server.password}";
 passwordAdmin = "${config.server.adminPassword}";
 maxPlayers = ${config.server.maxPlayers};
@@ -10,15 +9,15 @@ verifySignatures = ${config.server.verifySignatures};
 forceSameBuild = ${config.server.forceSameBuild};
 disableVoN = ${config.server.disableVon};
 vonCodecQuality = ${config.server.vonCodecQuality};
-disable3rdPerson=${config.server.disable3rdPerson};
-disableCrosshair=${config.server.disableCrosshair};
-serverTime="${config.server.serverTime}";
-serverTimeAcceleration=${config.server.serverTimeAcceleration};
+disable3rdPerson = ${config.server.disable3rdPerson};
+disableCrosshair = ${config.server.disableCrosshair};
+serverTime = "${config.server.serverTime}";
+serverTimeAcceleration = ${config.server.serverTimeAcceleration};
 serverNightTimeAcceleration = ${config.server.serverNightTimeAcceleration};
-serverTimePersistent=${config.server.serverTimePersistent};
-guaranteedUpdates=${config.server.guaranteedUpdates};
-loginQueueConcurrentPlayers=${config.server.loginQueueConcurrentPlayers};
-loginQueueMaxPlayers=${config.server.loginQueueMaxPlayers};
+serverTimePersistent = ${config.server.serverTimePersistent};
+guaranteedUpdates = ${config.server.guaranteedUpdates};
+loginQueueConcurrentPlayers = ${config.server.loginQueueConcurrentPlayers};
+loginQueueMaxPlayers = ${config.server.loginQueueMaxPlayers};
 instanceId = ${config.server.instanceID};
 storageAutoFix = ${config.server.storageAutoFix};
 motd[] = {"", "${config.server.motd}"};
@@ -44,8 +43,8 @@ networkRangeClose = ${config.server.networkRangeClose};
 networkRangeNear = ${config.server.networkRangeNear};
 networkRangeFar = ${config.server.networkRangeFar};
 networkRangeDistantEffect = ${config.server.networkRangeDistantEffect};
-defaultVisibility=${config.server.defaultVisibility};
-defaultObjectViewDistance=${config.server.defaultObjectViewDistance};
+defaultVisibility = ${config.server.defaultVisibility};
+defaultObjectViewDistance = ${config.server.defaultObjectViewDistance};
 lightingConfig = ${config.server.lightingConfig};
 disablePersonalLight = ${config.server.disablePersonalLight};
 disableBaseDamage = ${config.server.disableBaseDamage};
